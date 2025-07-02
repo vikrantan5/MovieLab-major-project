d003718959ec65d58780bfc25158dd8562264e02
vikrant singh <MUMMY@LAPTOP-UJS101H5.(none)>
2025-07-02 15:52:18 +0530

    second-commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './store/Store.jsx'
+import { store } from './store/store.jsx'
 
 createRoot(document.getElementById('root')).render(
 
